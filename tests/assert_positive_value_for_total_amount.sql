b48f081ba4e31c11214a00b9ac42033dd24c1843
matias.lagos@sainsburys.co.uk <matias.lagos@sainsburys.co.uk>
2025-05-23 14:58:24 +0000

    finished documentation

--- tests/assert_positive_value_for_total_amount.sql
+++ tests/assert_positive_value_for_total_amount.sql
@@ -1,21 +1,21 @@
 -- Refunds have a negative amount, so the total amount should always be >= 0.
 -- Therefore return records where this isn't true to make the test fail.
+select
+  order_id,
+  sum(amount) as total_amount
+from {{ ref('stg_stripe__payments') }}
+group by 1
+having (total_amount < 0)
+
+
+
+-- with payments as (
+--     select * from {{ ref('stg_stripe__payments') }}
+-- )
 -- select
 --     order_id,
 --     sum(amount) as total_amount
--- from {{ ref('stg_stripe__payments') }}
--- group by 1
--- having total_amount >= 0
-
-
-
-with payments as (
-    select * from {{ ref('stg_stripe__payments') }}
-)
-select
-    order_id,
-    sum(amount) as total_amount
-from
-    payments
-group by order_id
-having total_amount < 0
+-- from
+--     payments
+-- group by order_id
+-- having total_amount < 0
